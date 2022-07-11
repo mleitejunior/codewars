d54569c55711e5b43715c9c3b69e3b3e8d9cbfae
Marcelo Leite Junior <mleitejunior@gmail.com>
2022-07-10 21:34:32 -0300

    Updated new problem

---
+++ src/main/java/br/com/crazydevguy/kata/PowersOfTwo.java
@@ -0,0 +1,37 @@
+package br.com.crazydevguy.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static java.util.stream.LongStream.rangeClosed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PowersOfTwo {
+
+    // Code from problem https://www.codewars.com/kata/57a083a57cb1f31db7000028
+
+    // Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2
+    // with the exponent ranging from 0 to n ( inclusive ).
+    @Test
+    void codeToWork() {
+        assertArrayEquals(new long[]{1}, powersOfTwo(0));
+        assertArrayEquals(new long[]{1,2}, powersOfTwo(1));
+        assertArrayEquals(new long[]{1,2,4,8,16}, powersOfTwo(4));
+    }
+
+    // FIRST SOLUTION
+    public static long[] powersOfTwo(int n){
+        long[] result = new long[n+1];
+        for (int i = 0; i < n+1; i++) {
+            result[i] = (long) Math.pow(2.0, i);
+        }
+        return result;
+    }
+
+    // BETTER SOLUTION
+    public static long[] powersOfTwoBetterSolution(int n){
+        return rangeClosed(0, n).map(i -> (long) Math.pow(2, i)).toArray();
+    }
+
+}
